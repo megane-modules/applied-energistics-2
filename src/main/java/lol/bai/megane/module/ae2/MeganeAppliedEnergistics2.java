95574b239c3453891ecc25d117fb9f3cb341b340
deirn <deirn@bai.lol>
2022-01-28 21:25:27 +0700

    only show energy tooltip on energy cells other IAEPowerStorage pulls energy from the network on demand

--- src/main/java/lol/bai/megane/module/ae2/MeganeAppliedEnergistics2.java
+++ src/main/java/lol/bai/megane/module/ae2/MeganeAppliedEnergistics2.java
@@ -1,15 +1,13 @@
 package lol.bai.megane.module.ae2;
 
-import appeng.api.networking.energy.IAEPowerStorage;
 import appeng.blockentity.AEBaseInvBlockEntity;
 import appeng.blockentity.misc.InscriberBlockEntity;
 import appeng.blockentity.misc.VibrationChamberBlockEntity;
-import appeng.blockentity.networking.EnergyAcceptorBlockEntity;
+import appeng.blockentity.networking.EnergyCellBlockEntity;
 import lol.bai.megane.api.MeganeModule;
 import lol.bai.megane.api.registry.ClientRegistrar;
 import lol.bai.megane.api.registry.CommonRegistrar;
 import lol.bai.megane.module.ae2.provider.BaseItemProvider;
-import lol.bai.megane.module.ae2.provider.EnergyAcceptorEnergyProvider;
 import lol.bai.megane.module.ae2.provider.InscriberProgressProvider;
 import lol.bai.megane.module.ae2.provider.PowerStorageEnergyProvider;
 import lol.bai.megane.module.ae2.provider.VibrationChamberProgressProvider;
@@ -18,8 +16,9 @@ public class MeganeAppliedEnergistics2 implements MeganeModule {
 
     @Override
     public void registerCommon(CommonRegistrar registrar) {
-        registrar.addEnergy(IAEPowerStorage.class, new PowerStorageEnergyProvider<>());
-        registrar.addEnergy(EnergyAcceptorBlockEntity.class, new EnergyAcceptorEnergyProvider());
+        //registrar.addEnergy(IAEPowerStorage.class, new PowerStorageEnergyProvider<>());
+        //registrar.addEnergy(EnergyAcceptorBlockEntity.class, new EnergyAcceptorEnergyProvider());
+        registrar.addEnergy(EnergyCellBlockEntity.class, new PowerStorageEnergyProvider<>());
 
         registrar.addItem(AEBaseInvBlockEntity.class, new BaseItemProvider());
 
